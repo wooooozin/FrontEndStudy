5ab9f2312fc9332be9f7229e28e223c279d35a4c
wooooozin <95316662+wooooozin@users.noreply.github.com>
2023-12-27 06:44:53 +0900

    removed cached

--- JS2/main.js
+++ JS2/main.js
@@ -92,8 +92,14 @@ switch (num) {
 
 // for 
 const ulEl = document.querySelector('ul')
-for (let i = 0; i < 3; i+=1) {
+for (let i = 0; i < 10; i+=1) {
   const li = document.createElement('li');
   li.textContent = `list-${i + 1}`;
+  if ((i+1) % 2 === 0 ) {
+  li.addEventListener('click', function() {
+    console.log(li.textContent);
+  });
+  }
   ulEl.appendChild(li);
 }
+
