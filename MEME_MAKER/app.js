957f959167be072a858d07422cbdabfa23875e27
wooooozin <95316662+wooooozin@users.noreply.github.com>
2024-05-30 05:21:08 +0900

    Rect

--- MEME_MAKER/app.js
+++ MEME_MAKER/app.js
@@ -1,1 +1,10 @@
-alert("hh");
+const canvas = document.querySelector("canvas");
+const ctx = canvas.getContext("2d");
+
+canvas.width = 800;
+canvas.height = 800;
+
+ctx.rect(50, 50, 100, 200);
+ctx.fill();
+ctx.fillStyle = "red";
+ctx.fill(); 
